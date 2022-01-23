d51f3e5ed7c88639964d120c5bd9827727c994c4
Roman <romanchebotarenko@gmail.com>
2022-01-23 03:23:34 +0200

    Update app.js

--- practice-oop-01-starting-setup/practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,61 @@
-class Tooltip { }
+class Tooltip {}
 
-class ProjectItem { }
+class ProjectItem {
+  constructor(id, updateProjectListsFunction) {
+    this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitcherButton();
+  }
 
-class ProjectList { }
+  connectMoreInfoButton() {}
+  connectSwitcherButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click", this.updateProjectListsHandler)
+  }
+}
 
-class App { }
+class ProjectList {
+  projects = [];
 
+  constructor(type) {
+    this.type = type;
+
+    const projItems = document.querySelectorAll(`#${type}-projects li`);
+
+    for (const projItem of projItems) {
+      this.projects.push(
+        new ProjectItem(projItem.id, this.switchProject.bind(this))
+      );
+    }
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject() {
+    console.log(this);
+  }
+
+  switchProject(projectId) {
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
+  }
+}
+
+App.init();
